cfbb06f74dfd520e9177a9c175e4ef2cf43a3753
Andrew Schnieder <andrewschnieder@gmail.com>
2015-07-03 11:13:25 -0700

    comment out jquery

--- public/site.js
+++ public/site.js
@@ -1,5 +1,5 @@
-<script type="text/javascript">
-  $(document).ready(function() {
-    $(".fancybox").fancybox();
-  });
-</script>
+// <script type="text/javascript">
+//   $(document).ready(function() {
+//     $(".fancybox").fancybox();
+//   });
+// </script>
